ef98c89301c5e25d21322fd4b2de1556e71db394
Inoubli-Htm <inoubli.h@gmail.com>
2023-03-02 11:46:21 +0000

    project hooks todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddTodo from "./Component/AddTodo";
+import ListTodo from "./Component/ListTodo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "learn React", id: 1, done: false },
+    { text: "learn Node", id: 2, done: false },
+  ]);
+
+  // Add Todo
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  //delete Todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  //done Todo
+  const doneTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  // Edit Todo
+  const editTodo = (id, newText) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo))
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Workshop Hooks</h1>
+      <AddTodo addTodo={addTodo} />
+      <ListTodo
+        todos={todos}
+        deleteTodo={deleteTodo}
+        doneTodo={doneTodo}
+        editTodo={editTodo}
+      />
     </div>
   );
 }
